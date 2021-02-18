fc44bb2b7f7338106c9f2c4eff41ebe14c2a7cce
Nicolas Diot <nicolasdiot10@gmail.com>
2021-02-18 19:05:53 +0100

    add menu for desktop

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,89 @@
+import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+
 export default function Navbar() {
+
+    useEffect(function openMenu() {
+        const nav = document.querySelector('nav');
+        const menu = document.querySelector('.menu');
+        const title = document.querySelector('.title p');
+        const hamburgerOne = document.querySelector('.nav__hamburger--1'); 
+        const hamburgerTwo = document.querySelector('.nav__hamburger--2'); 
+        const hamburgerThree = document.querySelector('.nav__hamburger--3');  
+
+        nav.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            nav.classList.toggle('nav-black');
+            title.classList.toggle('white');
+            hamburgerOne.classList.toggle('bg-white');
+            hamburgerTwo.classList.toggle('bg-white');
+            hamburgerThree.classList.toggle('bg-white');
+        })
+    });
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+
     return (
         <>
-            <nav>
-                <div id="nav__menu">
-                    <a>
-                        <div className="nav__hamburger"></div>
-                        <div className="nav__hamburger nav__hamburger--smaller"></div>
-                    </a>
+            <Link href="/">
+                <a>
+                    <nav>
+                        <div className="title">
+                            <p>nd</p>
+                        </div>
+                        <div id="nav-hamburger">
+                            <div className="nav__hamburger--1"></div>
+                            <div className="nav__hamburger--2"></div>
+                            <div className="nav__hamburger--3"></div>
+                        </div>
+                    </nav>
+                </a>
+            </Link>
+            <div className="menu">
+                <div className="menu__section">
+                    <Link href="/" className="menu__info background-black" onClick={closeMenu}>
+                        <a>
+                            <h3 className="menu__title">Accueil</h3>
+                        </a>
+                    </Link>
                 </div>
-            </nav>
+                <div className="menu__section">
+                    <Link href="/services" className="menu__info background-black" onClick={closeMenu}>
+                        <a>
+                            <h3 className="menu__title">Services</h3>
+                        </a>
+                    </Link>
+                </div>
+                <div className="menu__section">
+                    <Link href="/portfolio" className="menu__info background-black" onClick={closeMenu}>
+                        <a>
+                            <h3 className="menu__title">Portfolio</h3>
+                        </a>
+                    </Link>
+                </div>
+                <div className="menu__section">
+                    <Link href="/apropos" className="menu__info background-black" onClick={closeMenu}>
+                        <a>
+                            <h3 className="menu__title">À propos</h3>
+                        </a>
+                    </Link>
+                </div>
+                <div className="menu__section">
+                    <Link href="/contact" className="menu__info background-black" onClick={closeMenu}>
+                        <a>
+                            <h3 className="menu__title">Contact</h3>
+                        </a>
+                    </Link>
+                </div>
+                <div className="menu__section">
+                    <div className="menu__info background-black">
+                        {/* <a href="/" className="hamburger hamburger--closed">Contenttochange</a> */}
+                        <p className="menu__menu">Menu</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Navbar from '../components/navbar'
-import Grid from '../components/grid'
 import Footer from '../components/footer'
-import VanillaTilt from 'vanilla-tilt';
+import VanillaTilt from 'vanilla-tilt'
 
 export default function Home() {
+
+
 
     return (
         <>
@@ -18,100 +19,98 @@ export default function Home() {
                 />
                 <script type="text/javascript" src="vanilla-tilt.js"></script>
             </Head>
-            <Grid />
             <Navbar />
-            <div id="title">
-                <p>nd nicolas</p>
+            <div className="container-content">
+                <main>
+                    <header className="header">
+                        <div className="header__img">
+                            <img src="images/mac-setup.jpg" alt="mac setup banner"/>
+                        </div>
+                        <div className="header__title">
+                            <h1>Ensemble développons votre site internet</h1>
+                        </div>
+                    </header>
+                    <div className="container">
+                        <div id="discover">
+                            <p>Découvrir</p>
+                            <span className="arrow"></span>
+                        </div>
+                        <div className="container container--grid">
+                            <article className="service-description">
+                                <h2>Esthétique</h2>
+                                <p>En tant que développeur front-end, je porte un intérêt prononcé pour l'esthétique
+                                et l'univers graphique de votre site. Chaque site internet est unique, c'est pourquoi
+                                vous pourrez choisir entre un template WordPress ou une charte graphique personnalisé
+                                à votre image. Pour tous conseils esthétiques, je vous ferais part de mon expérience
+                                en UI et UX Design. 
+                                </p>
+                            </article>
+                            <article className="service-description service-description--middle">
+                                <h2>Performant</h2>
+                                <p>La performance d'un site est crucial, c'est pourquoi j'optimiserais son fonctionnement
+                                avec des plugins comme WP Rocket si vous choisissez un site WordPress. Pour un site 
+                                personnalisé, la structure du code respectera les normes W3C pour un meilleur référencement,
+                                et chaque image sera compressée sans perte de qualité, pour un chargement plus rapide de
+                                votre site. 
+                                </p>
+                            </article>
+                            <article className="service-description">
+                                <h2>Responsive Design</h2>
+                                <p>Tous les sites que je développe sont responsive design, c'est à dire qu'ils s'adaptent
+                                à tous support : un ordinateur, une tablette ou un téléphone.
+                                </p>
+                            </article>  
+                        </div>
+                        <section id="service-banner">
+                            <a href="/services">
+                                <h3>Découvrir les services</h3>
+                                <span className="arrow arrow--white"></span>
+                            </a>
+                        </section>
+                        <section className="container container--grid">
+                            <div id="realisation">
+                                <h2>Réalisations</h2>
+                            </div>
+                            <div className="card" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-scale="1.1">
+                                <div className="card__img">
+                                    <img src="images/ombelinedurey.jpg" alt="Ombeline Durey"/>
+                                </div>
+                                <div className="card__info">
+                                    <div className="card__info-title">
+                                        <h4>Ombeline Durey</h4>
+                                    </div>
+                                    <div className="card__info-description">
+                                        <p><small>marketing digital</small></p>
+                                    </div>
+                                    <div className="card__info-discover">
+                                        <p>voir le projet</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card--right" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-scale="1.1">
+                                <div className="card__img">
+                                    <img src="images/ac-studio-1.png" alt="Semi Office"/>
+                                </div>
+                                <div className="card__info">
+                                    <div className="card__info-title">
+                                        <h4>Semi Office</h4>
+                                    </div>
+                                    <div className="card__info-description">
+                                        <p><small>Galerie de design</small></p>
+                                    </div>
+                                    <div className="card__info-discover">
+                                        <p>voir le projet</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section className="contact-banner">
+                            <h3>Un projet ? Une question ? N'hésitez pas à <a href="/contact">me contacter</a></h3>
+                        </section>
+                    </div>
+                </main>
+                <Footer />
             </div>
-            <main>
-                <header className="header">
-                    <div className="header__img">
-                        <img src="images/mac-setup.jpg" alt="mac setup banner"/>
-                    </div>
-                    <div className="header__title">
-                        <h1>Ensemble développons votre site internet</h1>
-                    </div>
-                </header>
-                <div className="container">
-                    <div id="discover">
-                        <p>Découvrir</p>
-                        <span className="arrow"></span>
-                    </div>
-                    <div className="container container--grid">
-                        <article className="service-description">
-                            <h2>Esthétique</h2>
-                            <p>En tant que développeur front-end, je porte un intérêt prononcé pour l'esthétique
-                            et l'univers graphique de votre site. Chaque site internet est unique, c'est pourquoi
-                            vous pourrez choisir entre un template WordPress ou une charte graphique personnalisé
-                            à votre image. Pour tous conseils esthétiques, je vous ferais part de mon expérience
-                            en UI et UX Design. 
-                            </p>
-                        </article>
-                        <article className="service-description service-description--middle">
-                            <h2>Performant</h2>
-                            <p>La performance d'un site est crucial, c'est pourquoi j'optimiserais son fonctionnement
-                            avec des plugins comme WP Rocket si vous choisissez un site WordPress. Pour un site 
-                            personnalisé, la structure du code respectera les normes W3C pour un meilleur référencement,
-                            et chaque image sera compressée sans perte de qualité, pour un chargement plus rapide de
-                            votre site. 
-                            </p>
-                        </article>
-                        <article className="service-description">
-                            <h2>Responsive Design</h2>
-                            <p>Tous les sites que je développe sont responsive design, c'est à dire qu'ils s'adaptent
-                            à tous support : un ordinateur, une tablette ou un téléphone.
-                            </p>
-                        </article>  
-                    </div>
-                    <section id="service-banner">
-                        <a href="/services">
-                            <h3>Découvrir les services</h3>
-                            <span className="arrow arrow--white"></span>
-                        </a>
-                    </section>
-                    <section className="container container--grid">
-                        <div id="realisation">
-                            <h2>Réalisations</h2>
-                        </div>
-                        <div className="card" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-scale="1.1">
-                            <div className="card__img">
-                                <img src="images/ombelinedurey.jpg" alt="Ombeline Durey"/>
-                            </div>
-                            <div className="card__info">
-                                <div className="card__info-title">
-                                    <h4>Ombeline Durey</h4>
-                                </div>
-                                <div className="card__info-description">
-                                    <p><small>marketing digital</small></p>
-                                </div>
-                                <div className="card__info-discover">
-                                    <p>voir le projet</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card card--right" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-scale="1.1">
-                            <div className="card__img">
-                                <img src="images/ac-studio-1.png" alt="Semi Office"/>
-                            </div>
-                            <div className="card__info">
-                                <div className="card__info-title">
-                                    <h4>Semi Office</h4>
-                                </div>
-                                <div className="card__info-description">
-                                    <p><small>Galerie de design</small></p>
-                                </div>
-                                <div className="card__info-discover">
-                                    <p>voir le projet</p>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="contact-banner">
-                        <h3>Un projet ? Une question ? N'hésitez pas à <a href="/contact">me contacter</a></h3>
-                    </section>
-                </div>
-            </main>
-            <Footer />
         </>
     )
 }
